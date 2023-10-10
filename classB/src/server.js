601c355e2a0c9e3ac931bab69ee89a60d89ca834
Associate <Associate@roifmrwindt>
2023-10-10 12:07:17 +0530

    Added basic routing

--- classB/src/server.js
+++ classB/src/server.js
@@ -1,39 +1,16 @@
 const express = require('express');
-const axios = require('axios');
-
- 
-
 const app = express();
-//Need to put this in environment variables at a later time
-const PORT = 3000;
+const port = 3000;
 
- 
+// Import your controller classes
+const laptopController = require('./controllers/laptops');
 
-// Define a route that makes an Axios GET request to an external API
-app.get('/api/data', async (req, res) => {
-  try {
-    // Make an HTTP GET request using Axios
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
 
- 
+// Use Express Router for each controller
+app.use('classB/laptop', laptopController);
 
-    // Send the data from the external API as a response
-    res.json(response.data);
-  } catch (error) {
-    // Handle errors gracefully
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
- 
 
 // Start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-function testMethod(){
-  return true;
-}
-
-exports = {testMethod}

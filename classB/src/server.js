5e34f73538ae8f08de8b6be44d340c82e53e4d96
Associate <Associate@roifmrwindt>
2023-10-10 14:14:35 +0530

    made the book.js

--- classB/src/server.js
+++ classB/src/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3022;
 
 // Import your controller classes
 const laptopController = require('./controllers/laptops');
+const bookController= require('./controllers/books');
 
 
 // Use Express Router for each controller
 app.use('classB/laptop', laptopController);
+app.use('classB/book', bookController);
 
 
 // Start the Express server

311f9f11426f7c0b23cbaf083c14e1441f007805
Associate <Associate@roifmrwindt>
2023-10-11 09:23:56 +0530

    Integration basic

--- classB/src/controllers/laptops.js
+++ classB/src/controllers/laptops.js
@@ -2,8 +2,10 @@ const express = require('express');
 const axios=require('axios')
 const router = express.Router();
 // Define routes for bikes
-router.get('/all/location', (req, res) => {
-  // Handle the route logic for bikes
+router.get('/all/location',async  (req, res) => {
+    
+  const response=await axios.get('http://localhost:8080/food');
+  res.json(response)
   res.send('Get all bikes by location');
 });
 
@@ -12,17 +14,22 @@ router.get('/team', (req, res) => {
   res.send('Get bike team information');
 });
 
-router.get('/api/data', async (req, res) => {
-  try {
-    // Make an HTTP GET request using Axios
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-    // Send the data from the external API as a response
-    res.json(response.data);
-  } catch (error) {
-    // Handle errors gracefully
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+// Make sure to import Axios if you haven't already
+
+router.get('/api/data', (req, res) => {
+  // Make an HTTP GET request using Axios
+  axios.get('http://127.0.0.1:8080/food')
+    .then(response => {
+      console.log("Success response---");
+      // Send the data from the external API as a response
+      res.json(response.data);
+    })
+    .catch(error => {
+      // Handle errors gracefully
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
+
 
 
 function testMethod(){

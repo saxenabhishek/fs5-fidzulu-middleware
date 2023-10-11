1f46c73bc52cc075e28b5445a7975151374ecb94
Associate <Associate@roifmrwindt>
2023-10-11 14:32:07 +0530

    chckin

--- classB/src/controllers/books.js
+++ classB/src/controllers/books.js
@@ -2,37 +2,52 @@ const express = require("express");
 const axios = require("axios");
 const {logRequestResponse, createLoggerWithPrefix} = require("../utils/logger");
 const router = express.Router();
+const Constants = require("../Constants/constants");
+const ErrorMessages = require("../Constants/errorMessages");
 
 router.use(logRequestResponse);
 
 const logger = createLoggerWithPrefix("Book Service");
 
-router.get("/all/:location", async(req, resp) => {
+router.get(Constants.APPLICATION_ROUTES.BOOK_ROUTES.ALL_BOOKS, async(req, resp) => {
     let location = req.params.location;
+    console.log(location)
+    if(location != "IN" && location!="IE" && location!="US-NC"){
+        resp.status(Constants.HTTP_STATUS_CODE.PAGE_NOT_FOUND).json({
+            error: ErrorMessages.ERROR.PAGE_NOT_FOUND,
+            detail: ErrorMessages.DETAIL.UNKNOWN_COUNTRY
+        })
+        return;
+    }
     let backendResp;
     try{
-        backendResp = await axios.get("http://127.0.0.1:8080/food");
+        //TODO: Axios URL from env
+        backendUrl = `${Constants.ENV.HOST_BOOKS}/all/${location}`;
+        console.log(backendUrl);
+        
+        backendResp = await axios.get(backendUrl);
         resp.status(200).json(backendResp.data);
     } catch(e){
         logger.error("Could not connect to backend for getting book details. ERROR:\n"+e);
         resp.status(500).json({
-            error: "Internal Server Error",
-            detail: "Unable to connect to the backend"
+            error: ErrorMessages.ERROR.INTERNAL_SERVER_ERROR,
+            detail: ErrorMessages.DETAIL.BACKEND_CONNECTION_FAILURE
         });
     }
 });
 
-router.get("/team", async(req, resp) =>{
+router.get(Constants.APPLICATION_ROUTES.BOOK_ROUTES.BOOKS_TEAM, async(req, resp) =>{
 
     let backendResp;
     try{
-        backendResp = await axios.get("");
+        //TODO: Get axios URL from env
+        backendResp = await axios.get(Constants.ENV.HOST_BOOKS);
         resp.status(200).json(backendResp.data);
     } catch(e){
         logger.error("Could not connect to backend for getting book team details\n. ERROR:", e);
         resp.status(500).json({
-            error: "Internal Server Error",
-            detail: "Unable to connect to the backend"
+            error: ErrorMessages.ERROR.INTERNAL_SERVER_ERROR,
+            detail: ErrorMessages.DETAIL.BACKEND_CONNECTION_FAILURE
         });
     }
 });

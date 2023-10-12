cd3ed977a150328f24a19940fa92b1b25fe29728
Senthooran B <Senthooran.B@fmr.com>
2023-10-12 11:58:37 +0530

    Completed integration. Final version (PRE-BUG-DISCOVERY)

--- classB/src/Constants/constants.js
+++ classB/src/Constants/constants.js
@@ -7,6 +7,13 @@ const ENV = {
     HOST_LAPTOPS: process.env.HOST_LAPTOPS,
 }
 
+const HTTP_STATUS_CODE = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    PAGE_NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+}
 
 const APPLICATION_ROUTES = {
     LAPTOP_ROUTES:{
